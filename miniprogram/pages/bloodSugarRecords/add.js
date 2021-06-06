1b6b33a4a01f6a11722cf620fd7663e7b38600b8
10xjzheng <10xjzheng@gmail.com>
2021-06-06 10:55:04 +0800

    [feat]完成基本页面

--- miniprogram/pages/bloodSugarRecords/add.js
+++ miniprogram/pages/bloodSugarRecords/add.js
@@ -5,21 +5,74 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    items: [
+      {value: '随机', name: '随机', checked: 'true'},
+      {value: '凌晨3点', name: '凌晨3点'},
+      {value: '早餐前', name: '早餐前'},
+      {value: '早餐后', name: '早餐后'},
+      {value: '午餐前', name: '午餐前'},
+      {value: '午餐后', name: '午餐后'},
+      {value: '晚餐前', name: '晚餐前'},
+      {value: '晚餐后', name: '晚餐后'},
+      {value: '睡前', name: '睡前'},
+    ],
+    dateMinute: '',
+    timePeriod:'随机',
+    recordValue:'',
+    remark:'',
   },
+  radioChange(e) {
+    const items = this.data.items
+    for (let i = 0, len = items.length; i < len; ++i) {
+      items[i].checked = items[i].value === e.detail.value
+    }
 
+    this.setData({
+      timePeriod: e.detail.value
+    })
+  },
+  recordValueInput:function(e)
+  {
+    this.setData({
+      recordValue: e.detail.value
+    })
+  },
+  remarkInput:function(e)
+  {
+    this.setData({
+      remark: e.detail.value
+    })
+  },
+  bindFormSubmit:function() {
+      console.log(this.data.dateMinute,this.data.recordValue, this.data.timePeriod,this.data.remark)
+  },
+  /**
+   * 年月日时分选择类型的回调函数，可以在该函数得到选择的时间
+   */
+  selectDateMinuteChange(ev) {
+    this.setData({
+      dateMinute: ev.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    var myDate = new Date();
+    var month = myDate.getMonth() + 1;
+    month = month > 9 ? month: '0' + month;
+    var day = myDate.getDate() > 9 ? myDate.getDate() : '0' + myDate.getDate();
+    var datetime = myDate.getFullYear() + '-' + month + '-' + day +' ' + myDate.getHours() + ':' + myDate.getMinutes();
+    this.setData({
+      dateMinute:  datetime
+    })
   },
 
   /**
--- miniprogram/pages/bloodSugarRecords/list.js
+++ miniprogram/pages/bloodSugarRecords/list.js
@@ -16,7 +16,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      
   },
 
   /**
@@ -66,5 +66,10 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  addRecord: function () {
+    wx.navigateTo({
+      url: 'add',
+    })
   }
 })
